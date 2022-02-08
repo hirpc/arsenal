3ee5344244ab2c3fdede909ca349c8142bf39ab8
Jankin <houzhenkai@houzhenkai.com>
2022-02-09 00:39:25 +0800

    add readme

--- hihttp/hihttp_test.go
+++ hihttp/hihttp_test.go
@@ -9,7 +9,14 @@ import (
 
 func TestGet(t *testing.T) {
 	// 设置全局超时时间
-	Load(WithTimeout(time.Second))
+	Load(
+		WithTimeout(time.Second),
+		WithRetryCount(1),
+		WithRetryWait(time.Second),
+		WithRetryError(func(ctx context.Context, c hiclient) error {
+			return nil
+		}),
+	)
 	res, err := New().Get(context.Background(), "http://www.google.com")
 	if err != nil {
 		t.Error(1, err)
@@ -39,10 +46,12 @@ func TestGet(t *testing.T) {
 
 func TestPost(t *testing.T) {
 	// 发送一个post请求
-	New().Post(context.Background(),"http://www.yumontime.com/test/login","user_name","yumontime","password","123123")
+	New().Post(context.Background(), "http://www.yumontime.com/test/login", "user_name", "yumontime", "password", "123123")
 
 	//
-	New().Post(context.Background(),"http://www.yumontime.com/test/login",map[string]interface{}{
-		"user_name":"yumontime","password":"123123",
+	New().Post(context.Background(), "http://www.yumontime.com/test/login", map[string]interface{}{
+		"user_name": "yumontime", "password": "123123",
 	})
+
+	New().SetHeader(SerializationType,SerializationTypeJSON)
 }
